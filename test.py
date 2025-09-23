0da079716ffe2a33c93401874241d4f6b9681ee5
neonb77 <nathan.bendich@gradientboostedinvestments.com>
2025-09-23 12:00:58 -0400

    test.py: deleted commented code

--- test.py
+++ test.py
@@ -11,7 +11,6 @@ from pprint import pprint as p
 if __name__=="__main__":
 	print('First day working at Boosted.AI!')                      
 	p({i:i*i for i in range(15)})
-	#print('Hello World!')	
 	f=lambda x:x*x
 	print(f(9))
 
