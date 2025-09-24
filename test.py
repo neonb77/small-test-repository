81a746885cda11dc4c8471c6055a402f5ac204fd
neonb77 <nathan.bendich@gradientboostedinvestments.com>
2025-09-24 15:55:15 -0400

    test.py

--- test.py
+++ test.py
@@ -9,10 +9,10 @@ from pprint import pprint as p
 
 
 if __name__=="__main__":
-	print('First day working at Boosted.AI!')                      
-	p({i:i*i for i in range(15)})
 	f=lambda x:x*x
-	print(f(9))
+	#print(f(9))
+	print('First day working at Boosted.AI!   Math: ', f(9) )                      
+	p({i:i*i for i in range(15)})
 
 
 
